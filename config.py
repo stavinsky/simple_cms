bc464eba1c03972a6728bd3660a941b30342b65f
stavinsky <stavinsky@gmail.com>
2016-06-21 18:16:06 +0300

    add first test

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
+import os
+
+
+BASEDIR = os.path.abspath(__file__)
+
 DB_CONNECTION_STRING = 'mongodb://192.168.99.100:32768'
---
+++ contrib/models/tests/test_models.py
@@ -0,0 +1,7 @@
+
+from contrib.models.validators import date_time_type_validator
+
+
+class test_model:
+    def test_date_tyme_validator():
+        assert True
