f07e1c3c94fe750ab1ace1fc7fcbac433d204d3d
stavinsky <stavinsky@gmail.com>
2016-06-21 18:30:38 +0300

    test validator

--- contrib/models/tests/test_models.py
+++ contrib/models/tests/test_models.py
@@ -1,7 +1,13 @@
-
+import pytest
 from contrib.models.validators import date_time_type_validator
+from datetime import datetime
 
 
-class test_model:
-    def test_date_tyme_validator():
-        assert True
+class TestModel:
+    def test_date_tyme_validator(self):
+        data = datetime.now()
+        date_time_type_validator(data)
+
+        with pytest.raises(Exception) as excinfo:
+            data = "not a time"
+            date_time_type_validator(data)
